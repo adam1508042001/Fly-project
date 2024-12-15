946e0bd084b21ad5ac7f7ad6f3f023ba3b53b7c4
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-12-15 14:53:50 +0100

    initialisation de projet vue

---
+++ front/vue/src/App.vue
@@ -0,0 +1,14 @@
+<template >
+  <div>
+    
+    <h2 class="text-3xl font-bold underline flex justify-center">
+      helloooooooooooooooooooooooooooooo
+    </h2>
+  </div>
+</template>
+<script>
+
+</script>
+<style >
+  
+</style>
